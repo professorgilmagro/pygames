d8e38a24330401fd3595342dcefcb9e52984944e
Gilmar Soares <professorgilmagro@gmail.com>
2014-08-30 01:05:29 -0300

    Lição 01 - Conceitos básicos

---
+++ lesson-01/README.md
@@ -0,0 +1,9 @@
+Lição 01
+===========
+
+Nesta primeira etapa vamos aprender conceitos básico de uso do pygame, tais como:
+
+- Criar a tela (screen) e determinar o tamanho (mode)
+- Alterar o fundo (background)
+- Adicionar comportamentos (eventos) básicos de mouse (DOWN and UP)
+- Prever a saída do game a partir do pressionamento de teclas
---
+++ lesson-01/game.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+# Gilmar S. Santos <gilmar.pythonman@outlook.com>
+
+import pygame
+import random
+
+
+def get_random_color():
+    """
+        Retorna uma cor aleatória em RGB
+    """
+    return(
+        random.randint(0, 255),
+        random.randint(0, 255),
+        random.randint(0, 255)
+    )
+
+
+def main():
+    pygame.init()
+
+    # cria uma tela no tamanho 600 x 480
+    screen = pygame.display.set_mode((600, 480))
+    pygame.display.set_caption(
+        "Tela do jogo - Clique para mudar a cor de fundo"
+    )
+
+    # Cor inicial
+    color = (255, 0, 0)
+
+    while True:
+        event = pygame.event.wait()
+
+        screen.fill(color)
+        pygame.display.flip()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            color = get_random_color()
+
+        elif event.type == pygame.QUIT:
+            pygame.quit()
+            return
+
+        elif event.type == pygame.MOUSEBUTTONUP:
+            pygame.display.set_caption("Cor atual RGB {0}".format(str(color)))
+
+
+if __name__ == "__main__":
+    main()
